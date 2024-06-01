94af2f666d2a07134d651f202f82d4a6877c68cb
rdawson46 <dawson4623@gmail.com>
2024-06-01 17:11:10 -0400

    more fugitive keys

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,3 +2,11 @@ vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
 vim.keymap.set('n', '<leader>gl', function()
     vim.cmd("Git log --graph --decorate")
 end)
+
+vim.keymap.set('n', '<leader>gP', function()
+    vim.cmd("Git pull")
+end)
+
+vim.keymap.set('n', '<leader>gp', function()
+    vim.cmd("Git push")
+end)
