9e9e9ccc5b09ed0e138903e9995275326dbba60e
rdawson46 <dawson4623@gmail.com>
2025-01-06 15:55:48 -0500

    fix

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
 require("second.lazy")
 require('first.remap')
 require('first.set')
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function ()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
