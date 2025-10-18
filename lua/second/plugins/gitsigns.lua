42a536e9b4904e726a124d30abd055ecb755ae35
rdawson46 <dawson4623@gmail.com>
2025-10-18 00:00:54 -0400

    inline preview

--- lua/second/plugins/gitsigns.lua
+++ lua/second/plugins/gitsigns.lua
@@ -1,6 +1,12 @@
 return {
     "lewis6991/gitsigns.nvim",
     config = function()
-        require('gitsigns').setup({})
+        require('gitsigns').setup({
+            on_attach = function (bufnr)
+                local gitsigns = require('gitsigns')
+
+                vim.keymap.set('n', '<leader>lp', gitsigns.preview_hunk_inline)
+            end
+        })
     end
 }
