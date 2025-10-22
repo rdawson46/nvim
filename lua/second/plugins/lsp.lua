ea3fad48c7de1cf0c1c91a68a11b1936a8de530c
rdawson46 <dawson4623@gmail.com>
2025-10-21 21:20:53 -0400

    lsp config for 0.11

--- lua/second/plugins/lsp.lua
+++ lua/second/plugins/lsp.lua
@@ -1,7 +1,6 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "VonHeikemen/lsp-zero.nvim",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
@@ -11,38 +10,58 @@ return {
     },
 
     config = function()
-        -- look into stevearc/conform.nvim
+        require('fidget').setup({})
 
-        local lsp_zero = require('lsp-zero')
+        vim.api.nvim_create_autocmd('LspAttach', {
+             group = vim.api.nvim_create_augroup('lsp_attach_group', { clear = true }),
+             callback = function (event)
+                local opts = {buffer = event.buf, remap=false}
 
-        lsp_zero.on_attach(function(client, bufnr)
-            local opts = {buffer = bufnr, remap=false}
-
-            vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
-        end)
-
-        lsp_zero.setup_servers({'rust_analyzer'})
+                vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+             end
+        })
 
         require('mason').setup({})
         require('mason-lspconfig').setup({
             ensure_installed = {'lua_ls'},
-            handlers = {
-                lsp_zero.default_setup,
-                lua_ls = function()
-                    local lua_opts = lsp_zero.nvim_lua_ls()
-                    require('lspconfig').lua_ls.setup(lua_opts)
-                end,
-            }
+            automatic_enable = true
         })
+
+    local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server'
+
+    local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+    local vue_plugin = {
+      name = '@vue/typescript-plugin',
+      location = vue_language_server_path,
+      languages = { 'vue' },
+      configNamespace = 'typescript',
+    }
+
+    local ts_ls_config = {
+      init_options = {
+        plugins = {
+          vue_plugin,
+        },
+      },
+      filetypes = tsserver_filetypes,
+    }
+
+    -- If you are on most recent `nvim-lspconfig`
+    local vue_ls_config = {}
+
+    -- nvim 0.11 or above
+    vim.lsp.config('vue_ls', vue_ls_config)
+    vim.lsp.config('ts_ls', ts_ls_config)
+    vim.lsp.enable({'ts_ls', 'vue_ls'}) -- If using `ts_ls` replace `vtsls` to `ts_ls`
 
         local cmp = require('cmp')
         local cmp_select = {behavior = cmp.SelectBehavior.Select}
@@ -53,14 +72,14 @@ return {
                 {name = 'nvim_lsp'},
                 {name = 'nvim_lua'},
             },
-            formatting = lsp_zero.cmp_format(),
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<Tab>'] = cmp.mapping.confirm({ select = true }),
                 ['<C-Space>'] = cmp.mapping.complete(),
 
-            })
+            }),
+            -- formatting = lsp_zero.cmp_format(),
         })
     end
 }
