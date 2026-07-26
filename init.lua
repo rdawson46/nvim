81ab572e54de09e23bdb1b61c33408d669d029d2
rdawson46 <dawson4623@gmail.com>
2026-07-26 13:41:25 -0400

    opam pathing

--- init.lua
+++ init.lua
@@ -2,6 +2,11 @@ require("second.lazy")
 require('first.remap')
 require('first.set')
 
+local opam_share = vim.fn.system("opam var share 2>/dev/null"):gsub("%s+$", "")
+if opam_share ~= "" then
+    vim.opt.runtimepath:prepend(opam_share .. "/ocp-indent/vim")
+end
+
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
 
